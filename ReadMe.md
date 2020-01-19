283853040b1ddf69a58f22cd745eca4ce424af0c
kamabery <kamabery@gmail.com>
2020-01-19 11:12:47 -0500

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+#How to use this project
+1. Register any dependencies in Program.RegisterServices
+2. Open MyService.cs
+3. Update constructor with any new dependencies.
+4. Modify Run()
+
---
+++ Services/MyService.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Kam.Console.Services
+{
+    public class MyService : IMyService
+    {
+        private readonly string _baseUrl;
+        private readonly string _token;
+        private readonly ILogger<MyService> _logger;
+
+        public MyService(ILoggerFactory loggerFactory, IConfigurationRoot config)
+        {
+            var baseUrl = config["SomeConfigItem:BaseUrl"];
+            var token = config["SomeConfigItem:Token"];
+
+            _baseUrl = baseUrl;
+            _token = token;
+            _logger = loggerFactory.CreateLogger<MyService>();
+        }
+
+        public async Task Run()
+        {
+            _logger.LogInformation(_baseUrl);
+            _logger.LogInformation(_token);
+        }
+    }
+}
